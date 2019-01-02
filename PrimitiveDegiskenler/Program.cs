3e98c95b694c33342b7f3ea00bd580890b3dcf91
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 12:54:16 +0300

    Add project files.

---
+++ PrimitiveDegiskenler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveDegiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor onRenk = ConsoleColor.White;
+            ConsoleColor ArkaRenk = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = ArkaRenk;
+            Console.ForegroundColor = onRenk;
+
+
+            int x = 100;
+            decimal y = 100;
+            bool deger = true;
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}
